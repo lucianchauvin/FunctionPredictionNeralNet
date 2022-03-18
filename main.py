54d79fd66c475147e020016df6bb454b2abd9840
Lucian Chauvin <lucianchauvin@gmail.com>
2022-03-18 13:45:27 -0500

    neural net, I have no clue what I am doing!

--- main.py
+++ main.py
@@ -8,19 +8,28 @@
 '''
 
 from scipy.special import gamma
+import matplotlib.pyplot as plt
 import math
+import csv
 
 
-def genData(n, step):
+# Generates data for gamma function from n which is the maximum and step which is the step size
+def genGammaData(n, step, nameloc):
     if n > 170:
         raise ValueError("n must be less than 170")
+    # Generates values for the x axis (inputs)
     values = [x*step for x in range(int(n/step))]
     nv = []
     for x in values:
+        # fixes the fact that computers think .1 + .2 is .3000000000000000004
         nv.append(round(x, abs(round(math.log(step, 10)))))
-    with open('gamma_0-170_E5.txt', 'w') as f:
+    with open(nameloc, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
         for x in nv:
-            f.write(str(x) + ' ' + str(gamma(x+1)) + "\n")
+            # Writes the x and y values to the file, eventually this will be generalized to any function
+            writer.writerow([str(x), str(gamma(x+1))])
 
 
-genData(170, .00001)
+# genGammaData(170, .0001, "data/gamma_0-170_E4.csv")
+# genGammaData(170, .00001, "data/gamma_0-170_E5.csv")
+# genGammaData(170, .000001, "data/gamma_0-170_E6.csv")
