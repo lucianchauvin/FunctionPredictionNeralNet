5b2faabdef2ba001ce4a490a7d26beabb91caaef
Lucian Chauvin <lucianchauvin@gmail.com>
2022-03-19 00:33:44 -0500

    cleanup and comments

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ import matplotlib.pyplot as plt
 import math
 import csv
 import keras
-import keras.backend as K
 import tensorflow as tf
+import keras.backend as K
+from keras.utils.vis_utils import plot_model
 import numpy as np
-
 
 # Generates data for gamma function from n which is the maximum and step which is the step size, turns out this can be done a lot easier with nump. Also traning an Neural Net off of this data might be a bad idea due to how the inputs are not random.
 
@@ -56,13 +56,14 @@ def genData(min, max):
 # These two functions are causing problems, although it still works pretty well
 
 
-def sigmoid(n, min, max):  # Non-Linear normilization,  activation function (idk what that means yet but its gonna need to be changed later to be more accurate) ERROR - there is some inacuraccy in this functon thats causing some problems. I am pretty sure this is why I'm not getting too acurate of results.
+def sigmoid(n, min, max):  # Non-Linear normilization, WHERE MIN MAX ARE ABSOLUTE MIN AND MAX ON THE INTERVAL
     var1 = 8/(max-min)
     var2 = n-((max+min)/2)
     return 1/(1+K.exp(-var1*var2))
 
 
-def sigmoidInverse(n, min, max):  # Inverse of sigmoid
+# Inverse of sigmoid, WHERE MIN MAX ARE ABSOLUTE MIN AND MAX ON THE INTERVAL
+def sigmoidInverse(n, min, max):
     return math.log((1/n)-1)/(-8/(max-min)) + (max+min)/2
 
 
@@ -89,13 +90,14 @@ model = keras.Sequential([
 ])
 
 sgd = tf.keras.optimizers.SGD(0.01, momentum=0.9)
-model.compile(optimizer=sgd, loss="mse", metrics=['accuracy'])
+model.compile(optimizer=sgd, loss="mse")
 
-history = model.fit(x=x, y=y, epochs=3, batch_size=64,
+history = model.fit(x=x, y=y, epochs=1, batch_size=64,
                     validation_data=(x_test, y_test))
 
 model.save('model.h5')
 
 reloaded = keras.models.load_model('model.h5')
+
 
 print(sigmoidInverse(reloaded.predict([4.8]), func(min), func(max)))
